7413fb4253862476ba3b8a5523b13c376c49bb9b
rnd-pai <ksorn@kku.ac.th>
2016-09-14 10:18:52 +0700

    Re format

--- FizzBuzzTest.php
+++ FizzBuzzTest.php
@@ -8,7 +8,7 @@ class FizzBuzz
 		$this->value = $value;
 	}
 
-	function toString(){
+	function count(){
 		if (($this->value %15)==0) 
 		{
 			return "FizzBuzz";
@@ -30,26 +30,26 @@ class FizzBuzzTest extends PHPUnit_Framework_TestCase
 {
 	function testShouldBeNumberOne(){
 		$fizzbuzz = new FizzBuzz(1);
-		$this->assertEquals("1", $fizzbuzz->toString());
+		$this->assertEquals("1", $fizzbuzz->count());
 	}
 	function testShouldBeNumberTwo(){
 		$fizzbuzz = new FizzBuzz(2);
-		$this->assertEquals("2", $fizzbuzz->toString());
+		$this->assertEquals("2", $fizzbuzz->count());
 	}
 	function testShouldBeReturnFizzString()
 	{
 		$fizzbuzz = new FizzBuzz(3);
-		$this->assertEquals("Fizz", $fizzbuzz->toString());
+		$this->assertEquals("Fizz", $fizzbuzz->count());
 	}
 	function testShouldBeReturnBuzzString()
 	{
 		$fizzbuzz = new FizzBuzz(5);
-		$this->assertEquals("Buzz", $fizzbuzz->toString());
+		$this->assertEquals("Buzz", $fizzbuzz->count());
 	}
 	function testShouldBeReturnFizzBuzzString()
 	{
 		$fizzbuzz = new FizzBuzz(15);
-		$this->assertEquals("FizzBuzz", $fizzbuzz->toString());
+		$this->assertEquals("FizzBuzz", $fizzbuzz->count());
 	}
 }
 ?>
