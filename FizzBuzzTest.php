89f18ae306109914d91e089f29606dad9fda853d
rnd-pai <ksorn@kku.ac.th>
2016-09-14 09:43:41 +0700

    Test Should be number two

--- FizzBuzzTest.php
+++ FizzBuzzTest.php
@@ -10,7 +10,9 @@ class FizzBuzz
 	}
 
 	function toString(){
-		return "1";
+		if($this->value == 1)
+			return "1";
+		return "2";
 	}
 }
 
@@ -23,6 +25,12 @@ class FizzBuzzTest extends PHPUnit_Framework_TestCase
 
 		$fizzbuzz = new FizzBuzz(1);
 		$this->assertEquals("1", $fizzbuzz->toString());
+	}
+
+	function testShouldBeNumberTwo(){
+
+		$fizzbuzz = new FizzBuzz(2);
+		$this->assertEquals("2", $fizzbuzz->toString());
 	}
 
 }
