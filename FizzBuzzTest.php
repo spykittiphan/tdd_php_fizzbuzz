a9077e10fbfe7bd1a52faa13a833e29d976cd6fa
rnd-pai <ksorn@kku.ac.th>
2016-09-14 09:54:06 +0700

    Test Should be return Fizz string

--- FizzBuzzTest.php
+++ FizzBuzzTest.php
@@ -17,7 +17,13 @@ class FizzBuzz
 		elseif($this->value==2)
 		{
 			return "2";
-		} elseif ($this->value==5) {
+		} 
+		elseif ($this->value==3) 
+		{
+			return "Fizz";
+		}
+		elseif ($this->value==5) 
+		{
 			return "Buzz";
 		}
 		
@@ -28,19 +34,20 @@ class FizzBuzz
 
 class FizzBuzzTest extends PHPUnit_Framework_TestCase
 {
-	
 	function testShouldBeNumberOne(){
-
 		$fizzbuzz = new FizzBuzz(1);
 		$this->assertEquals("1", $fizzbuzz->toString());
 	}
 
 	function testShouldBeNumberTwo(){
-
 		$fizzbuzz = new FizzBuzz(2);
 		$this->assertEquals("2", $fizzbuzz->toString());
 	}
 
+	function testShouldBeReturnFizzString(){
+		$fizzbuzz = new FizzBuzz(3);
+		$this->assertEquals("Fizz", $fizzbuzz->toString());
+	}
 	function testShouldBeReturnBuzzString(){
 		$fizzbuzz = new FizzBuzz(5);
 		$this->assertEquals("Buzz", $fizzbuzz->toString());
